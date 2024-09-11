b1d2f910c6f0fbc542b19323a12bc5044d292c77
Rishav <rishavjaiswal864@gmail.com>
2024-09-11 22:37:35 +0530

    check2

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,7 @@
 import SubHeading from './SubHeading/SubHeading';
 import Newsletter from './Footer/Newsletter';
 import FooterOverlay from './Footer/FooterOverlay';
-import MenuItem from './MenuItem/MenuItem';
+import MenuItem from './MenuItem/MenuItem.jsx';
 import Navbar from './Navbar/Navbar';
 
 export {
